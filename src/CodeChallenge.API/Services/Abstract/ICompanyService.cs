850811efcfc61e009b24ccb1be2aca5dc87463ac
Emanuel Paul <emanuel.paul@hotmail.com>
2019-06-17 17:22:17 +0300

    Add company service Add connection string setting Add db context in DI

---
+++ src/CodeChallenge.API/Services/Abstract/ICompanyService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CodeChallenge.API.Models;
+
+namespace CodeChallenge.API.Services.Abstract
+{
+    public interface ICompanyService
+    {
+        Task<IEnumerable<Company>> GetAllCompaniesAsync();
+
+        Task AddAsync(Company company);
+
+        Task<Company> GetByIdAsync(int companynId);
+
+        Task<Company> GetByIsinAsync(string isin);
+
+        Task UpdateAsync(int companyId, Company company);
+
+        Task DeleteAsync(int companynId);
+
+        Task<bool> Exists(int id);
+
+        Task<bool> Exists(string isin);
+    }
+}
---
+++ src/CodeChallenge.API/Services/Concrete/CompanyService.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CodeChallenge.API.Models;
+using CodeChallenge.API.Persistence;
+using CodeChallenge.API.Services.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeChallenge.API.Services.Concrete
+{
+    public class CompanyService : ICompanyService
+    {
+        private readonly CodeChallengeDbContext _dbContext;
+
+        public CompanyService(CodeChallengeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task AddAsync(Company company)
+        {
+            _dbContext.Companies.Add(company);
+
+            return _dbContext.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(int companynId)
+        {
+            _dbContext.Companies.Remove(new Company { CompanyId = companynId });
+
+            return _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Company>> GetAllCompaniesAsync() =>
+            await _dbContext.Companies.ToListAsync();
+
+        public Task<Company> GetByIdAsync(int companynId) =>
+            _dbContext.Companies.FindAsync(companynId);
+
+        public Task<Company> GetByIsinAsync(string isin) =>
+            _dbContext.Companies.FirstOrDefaultAsync(x => x.Isin == isin);
+
+        public Task UpdateAsync(int companyId, Company company)
+        {
+            company.CompanyId = companyId;
+            _dbContext.Companies.Update(company);
+
+            return _dbContext.SaveChangesAsync();
+        }
+
+        public Task<bool> Exists(int id) => _dbContext.Companies.AnyAsync(x => x.CompanyId == id);
+
+        public Task<bool> Exists(string isin) =>
+            _dbContext.Companies.AnyAsync(x => x.Isin == isin);
+    }
+}
