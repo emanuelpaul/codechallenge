16f7773aeb6c5a6587f4099e15c226e4efb7d6f2
Emanuel Paul <emanuel.paul@hotmail.com>
2019-06-17 17:09:40 +0300

    Add CompanyDto Add CompanyController Remove unused controller Added needed packages

---
+++ src/CodeChallenge.API/Controllers/CompaniesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeChallenge.API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeChallenge.API.Controllers
+{
+    [ApiController]
+    [Route("Companies")]
+    public class CompaniesController : ControllerBase
+    {
+        public ActionResult<IEnumerable<CompanyDto>> GetAll() =>
+            new[] { new CompanyDto { Name = "UiPath" } };
+    }
+}
---
+++ src/CodeChallenge.API/DTOs/CompanyDto.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeChallenge.API.DTOs
+{
+    public class CompanyDto
+    {
+        public int CompanyId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Exchange { get; set; }
+
+        public string Ticker { get; set; }
+
+        public string Isin { get; set; }
+
+        public string Website { get; set; }
+    }
+}
