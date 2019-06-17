548678379bcca83f6e96d989d00d134f3ad57faf
Emanuel Paul <emanuel.paul@hotmail.com>
2019-06-17 17:16:05 +0300

    Add company db map for ef Add EF db context

---
+++ src/CodeChallenge.API/Persistence/CodeChallengeDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CodeChallenge.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeChallenge.API.Persistence
+{
+    public class CodeChallengeDbContext : DbContext
+    {
+        public CodeChallengeDbContext(DbContextOptions<CodeChallengeDbContext> options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.LazyLoadingEnabled = false;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CompanyDbMap());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Company> Companies { get; set; }
+    }
+}
---
+++ src/CodeChallenge.API/Persistence/CompanyDbMap.cs
@@ -0,0 +1,20 @@
+﻿using CodeChallenge.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CodeChallenge.API.Persistence
+{
+    public class CompanyDbMap : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.ToTable("Company");
+            builder.ForSqlServerHasIndex(x => x.Isin).IsUnique();
+            builder.Property(x => x.Exchange).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Isin).IsRequired().HasMaxLength(12);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Ticker).IsRequired().HasMaxLength(5);
+            builder.Property(x => x.Website).HasMaxLength(120);
+        }
+    }
+}
