7c0916d77b1fa89ccf72007a752a964db4e394f0
Emanuel Paul <emanuel.paul@hotmail.com>
2019-06-17 17:12:57 +0300

    Add company model

---
+++ src/CodeChallenge.API/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeChallenge.API.Models
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Exchange { get; set; }
+
+        public string Ticker { get; set; }
+
+        public string Isin { get; set; }
+
+        public string Website { get; set; }
+    }
+}
